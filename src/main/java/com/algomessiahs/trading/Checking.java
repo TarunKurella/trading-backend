567de6d52323260acb11e990bdc670113befba92
tarun kurella <tarunkurella@gmail.com>
2021-09-16 21:47:26 +0530

    Adding Spring-boot files

---
+++ src/main/java/com/algomessiahs/trading/Checking.java
@@ -0,0 +1,13 @@
+package com.algomessiahs.trading;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Checking {
+
+    @GetMapping("check")
+    String checking(){
+        return "Backend Functional!";
+    }
+}
