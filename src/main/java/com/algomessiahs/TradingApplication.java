f3c79b01ac758cc28a8c501cb06fbf1fdc1e8438
TronoKurelz <34759498+TarunKurella@users.noreply.github.com>
2021-09-19 21:51:40 +0530

    Update and rename Trading1Application.java to TradingApplication.java

--- src/main/java/com/algomessiahs/TradingApplication.java
+++ src/main/java/com/algomessiahs/TradingApplication.java
@@ -6,19 +6,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @SpringBootApplication
-public class Trading1Application {
+public class TradingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Trading1Application.class, args);
+		SpringApplication.run(TradingApplication.class, args);
 	}
+	
 	@Bean
 	public WebMvcConfigurer corConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedHeaders("*").allowedMethods("*");
+				registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
 			}
 		};
 	}
+
 }
+
